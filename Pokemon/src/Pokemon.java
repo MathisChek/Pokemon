03fc9e0f47b9c1143f7c7fafd6357a3166749f0c
Mathis Chekraoui <mathis.chekraoui@hotmail.com>
2025-01-13 15:08:39 +0100

    Class pokemon initialised

--- Pokemon/src/Pokemon.java
+++ Pokemon/src/Pokemon.java
@@ -1,2 +1,35 @@
-public class Pokemon {
+import java.util.List;
+
+class Pokemon {
+    String name;
+    String type;
+    int level;
+    int healthPoints;
+    int maxHealthPoints;
+    List<Attaque> moves;
+
+    public Pokemon(String name, String type, int level, int maxHealthPoints, List<Attaque> moves) {
+        this.name = name;
+        this.type = type;
+        this.level = level;
+        this.maxHealthPoints = maxHealthPoints;
+        this.healthPoints = maxHealthPoints;
+        this.moves = moves;
+    }
+
+    // Attaque un autre Pokémon
+    public void attack(Pokemon target, Attaque move) {
+        int damage = move.damage;
+        System.out.println(name + " utilise " + move.name + " !");
+        target.healthPoints -= damage;
+
+        if (target.healthPoints < 0) target.healthPoints = 0;
+
+        System.out.println(target.name + " perd " + damage + " PV ! Il reste " + target.healthPoints + "/" + target.maxHealthPoints + " PV.");
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Type: " + type + ", Niveau: " + level + ", PV: " + healthPoints + "/" + maxHealthPoints + ")";
+    }
 }
